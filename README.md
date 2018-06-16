1893e607bfd34406838bbf99b9748ddc4aca185d
Moises Martinez <moimart@gmail.com>
2018-06-17 01:00:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+hyper-tab-touchbar
+======
+
+# Use your MacBook Pro's TouchBar to jump to your tabs
+
+# Install
+
+From hyper terminal: `hyper i hyper-tab-touchbar`
+
+# Features:
+
+- Show your open tabs in your MacBook Pro's TouchBar
+- Click on the tabs to jump to the specific tab
+- If you have [hyper-folder-icon](https://github.com/moimart/hyper-folder-icon) installed, you can your Volumes and custom folder's icons shown in the TouchBar
+
+## Optional Configuration keys
+
+- All inside hyperTouchBar object
+- activeColor -> hex rgb value string like '#005566' (that's the default value)
+- inactiveColor -> hex rgb value string like '#777777' (that's the default value)
+- specialButton -> Object containing a script path exporting an arrow function (window) => {} ; By default it 'clears' the active terminal
+
+```
+{
+  activeColor: '#005566',
+  inactiveColor: '#777777',
+  specialButton: {
+    label: 'clear',
+    onClick: 'path_to_file.js'
+  }
+```
+
+The script simply exports a function:
+
+```
+module.exports = () => {
+  console.log('Hello World');
+};
+```
+
+set specialButton to undefined if you don't want the special button to be shown
---
+++ index.js
@@ -0,0 +1,185 @@
+const { nativeImage } = require('electron');
+const { TouchBar } = require('electron');
+const { exec } = require('child_process');
+
+Array.prototype.diff = function(a) {
+    return this.filter(function(i) {return a.indexOf(i) < 0;});
+};
+
+let config = {
+  activeColor: '#005566',
+  inactiveColor: '#777777',
+  specialButton: {
+    label: 'clear',
+    onClick: () => {
+      window.store.dispatch({
+        type: 'SESSION_CLEAR_ACTIVE'
+      });
+    }
+  }
+};
+
+if (TouchBar) {
+  //We are inside the main process
+
+  const { TouchBarButton } = TouchBar;
+
+  exports.onWindow = (window) => {
+    if (window) {
+
+      window.rpc.on('touchbar-tabs', (tabs) => {
+        let buttons = [];
+
+        for (let data of tabs) {
+          if (data.iconData) {
+            data.icon = nativeImage.createFromBuffer(Buffer.from(data.iconData.buffer,'base64'));
+          } else {
+            delete data.icon;
+          }
+
+          if (!data.specialButton) {
+            data.click = () => {
+              window.rpc.emit('activate-tab-uid',data.sessionUid);
+            };
+          } else {
+            data.click = () => {
+              window.rpc.emit('activate-tab-special')
+            }
+          }
+
+          buttons.push(new TouchBarButton(data));
+        }
+
+        window.setTouchBar(new TouchBar(buttons));
+      });
+    }
+  }
+}
+
+var allTabs = {};
+var listener;
+
+let populateTouchBar = () => {
+
+  if (Object.keys(allTabs).length == 1) {
+    allTabs = {};
+
+    if (config.specialButton) {
+      let buttonsData = [{
+        label: config.specialButton.label,
+        specialButton: true,
+        backgroundColor: config.activeColor
+      }];
+
+      window.rpc.emit('touchbar-tabs',buttonsData);
+    }
+
+    return;
+  }
+
+  let buttonsData = [];
+
+  if (config.specialButton) {
+    buttonsData.push({
+      label: config.specialButton.label,
+      specialButton: true,
+      backgroundColor: config.activeColor
+    });
+  }
+
+  for (let uid of Object.keys(allTabs)) {
+    buttonsData.push({
+      sessionUid: window.store.getState().termGroups.termGroups[uid].sessionUid,
+      label: allTabs[uid].text,
+      iconData: (allTabs[uid].iconData) ? allTabs[uid].iconData : undefined,
+      backgroundColor: allTabs[uid].isActive ? config.activeColor : config.inactiveColor,
+      iconPosition: "left"
+    });
+  }
+
+  try {
+    window.rpc.emit('touchbar-tabs',buttonsData);
+  } catch(error) {
+
+  }
+
+  if (listener == undefined) {
+    window.rpc.on('activate-tab-uid', (uid) => {
+      window.store.dispatch({
+        type:'SESSION_SET_ACTIVE',
+        uid
+      });
+
+      populateTouchBar();
+    });
+
+    window.rpc.on('activate-tab-special', () => {
+      if (config.specialButton) {
+        config.specialButton.onClick();
+      }
+    });
+
+    listener = window;
+  }
+};
+
+exports.getTabProps = (uid,parentProps,props) => {
+  allTabs[uid.uid] = Object.assign({},allTabs[uid.uid],props);
+  return props;
+}
+
+exports.getTabsProps = (parentProps,props) => {
+  const { tabs } = props;
+
+  let existingTabs = {};
+  for (let tab of tabs) {
+    existingTabs[tab.uid] = tab;
+  }
+
+  let waste = Object.keys(allTabs).slice().diff(Object.keys(existingTabs));
+
+  for (let w of waste) {
+    delete allTabs[w];
+  }
+
+  populateTouchBar();
+
+  return props;
+}
+
+exports.middleware = (store) => (next) => (action) => {
+    const uids = store.getState().sessions.sessions;
+
+    switch (action.type) {
+        case 'FOLDER_ICON':
+          if (allTabs.hasOwnProperty(action.icon.uid)) {
+            allTabs[action.icon.uid].iconData = action.icon.iconData;
+            populateTouchBar();
+          }
+          break;
+
+        case 'SESSION_SET_ACTIVE':
+          populateTouchBar();
+          break;
+
+        case 'CONFIG_LOAD':
+    		case 'CONFIG_RELOAD':
+  			   if (action.config.hyperTouchBar) {
+             config = Object.assign({},config,action.config.hyperTouchBar);
+             if (action.config.hyperTouchBar.specialButton
+                 && action.config.hyperTouchBar.specialButton.onClick) {
+               try {
+                  let fn = require(config.specialButton.onClick);
+                  if (fn) {
+                    config.specialButton.onClick = fn;
+                  }
+               } catch (error) {
+                  //do nothing...
+               }
+             }
+           }
+  			   break;
+    }
+
+    next(action);
+};
